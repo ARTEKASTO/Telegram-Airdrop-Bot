242436fb214047840e74721f33739933055f719b
ARTEKASTO <86351729+ARTEKASTO@users.noreply.github.com>
2023-02-13 15:14:57 +0300

    hhh

--- utils/env.py
+++ utils/env.py
@@ -2,28 +2,28 @@ import os
 import pickle
 
 
-COIN_SYMBOL = os.environ["BAR"]
-COIN_NAME = os.environ["Blockar"]
-AIRDROP_AMOUNT = os.environ["500"]
+COIN_SYMBOL = os.environ["COIN_SYMBOL"]
+COIN_NAME = os.environ["COIN_NAME"]
+AIRDROP_AMOUNT = os.environ["AIRDROP_AMOUNT"]
 AIRDROP_AMOUNT = "{:,.2f}".format(float(AIRDROP_AMOUNT))
-AIRDROP_DATE = os.environ[" 25 March 2023"]
-BOT_TOKEN = os.environ["6098122086:AAEnn_6514tH2gNaf8RnMt0GlOeAi_Af0E4"]
+AIRDROP_DATE = os.environ["AIRDROP_DATE"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
 AIRDROP_NETWORK = os.environ["AIRDROP_NETWORK"]
-REFERRAL_REWARD = float(os.environ["50"])
-COIN_PRICE = os.environ["$0.1"]
-WEBSITE_URL = os.environ["http://blockar.net"]
-MONGO_USER = os.environ["banteam0"]
-MONGO_PASSWORD = os.environ["G0B5iJtJ9TB6Zfo3"]
-MONGO_IP = os.environ["88.241.79.130"]
-MONGO_PORT = os.environ["27017"]
-EXPLORER_URL = os.environ["https://data.mongodb-api.com/app/data-civnp/endpoint/data/v1"]
-ADMIN_USERNAME = os.environ["@onurartan"]
+REFERRAL_REWARD = float(os.environ["REFERRAL_REWARD"])
+COIN_PRICE = os.environ["COIN_PRICE"]
+WEBSITE_URL = os.environ["WEBSITE_URL"]
+MONGO_USER = os.environ["MONGO_INITDB_ROOT_USERNAME"]
+MONGO_PASSWORD = os.environ["MONGO_INITDB_ROOT_PASSWORD"]
+MONGO_IP = os.environ["MONGO_INITDB_IP"]
+MONGO_PORT = os.environ["MONGO_INITDB_PORT"]
+EXPLORER_URL = os.environ["EXPLORER_URL"]
+ADMIN_USERNAME = os.environ["ADMIN_USERNAME"]
 
-TWITTER_LINKS = os.environ["https://twitter.com/blockaret"]
-TELEGRAM_LINKS = os.environ["https://t.me/blockarnet"]
-MAX_USERS = int(os.environ["200000"])
-MAX_REFS = int(os.environ["20"])
-CAPTCHA_ENABLED = os.environ["yes"]
+TWITTER_LINKS = os.environ["TWITTER_LINKS"]
+TELEGRAM_LINKS = os.environ["TELEGRAM_LINKS"]
+MAX_USERS = int(os.environ["MAX_USERS"])
+MAX_REFS = int(os.environ["MAX_REFS"])
+CAPTCHA_ENABLED = os.environ["CAPTCHA_ENABLED"]
 
 TWITTER_LINKS = TWITTER_LINKS.split(",")
 TELEGRAM_LINKS = TELEGRAM_LINKS.split(",")
